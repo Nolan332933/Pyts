b1d6a2e9065d9cf1ddaa10609be030d2806539bb
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-09-12 19:07:04 +0630

    simple-bill-payment-logic

---
+++ bill-payment/main.py
@@ -0,0 +1,132 @@
+import datetime
+
+# Initialize an empty list to store payment history
+
+payment_history = []
+
+def display_menu():
+
+    print("\n- - Bill Payment App - -")
+
+    print("1. Pay a Bill")
+
+    print("2. View Payment History")
+
+    print("3. Exit")
+    
+    choice = input("Please select an option (1-3): ")
+
+    return choice
+
+def display_bills():
+
+    print("\n- - Availabe Bills - -")
+
+    print("1. Electricity Bill")
+
+    print("2. Water Bill")
+
+    print("3. Internet Bill")
+
+    print("4. Phone Bill")
+
+    bill_choice = input("Please select a bill to pay (1-4): ")
+
+    return bill_choice
+
+# funtion to handel the bill payment process.
+
+def pay_bills():
+
+    bill_choice = display_bills()
+
+    bill_name = ""
+
+    if bill_choice == "1":
+
+        bill_name = "Electricity bill"
+
+    elif bill_choice == "2":
+
+        bill_name = "Water bill"
+
+    elif bill_choice == "3":
+
+        bill_name = "Internet bill"    
+
+    elif bill_choice == "4":
+
+        bill_name = "Phone bill"
+
+    else: 
+
+        print("Invalid choice. Returning to main screen")
+
+        return
+    
+    amount = float(input(f"Enter the amount to pay for {bill_name}: $"))
+
+    confirm = input(f"Confirm payment of ${amount: .2f} for {bill_name}? (yes/no): "). lower()
+
+    if confirm == 'yes':
+
+        payment = {
+            'bill_name': bill_name,
+            "amount": amount,
+            "date": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+
+        payment_history.append(payment)
+
+        print(f"Payment of %{amount: .2f} for {bill_name} successful.")
+
+    else:
+
+        print("Pyament cancelled")
+
+#function to view payment histroy
+        
+def view_payment_history():
+     
+    if not payment_history:
+
+        print("\nNo payment history found")
+
+    else:
+
+        print("\n- - Payment History - -")
+
+        for payment in payment_history:
+
+            print (f"Bill: {payment["bill_name"]}, Amount: {payment["amount"]}, Date: {payment["date"]}")
+
+#Main application loop
+
+def main():
+
+    while True:
+
+        choice = display_menu
+
+        if choice == "1":
+
+            pay_bills()
+
+        elif choice == "2":
+
+            view_payment_history()
+        
+        elif choice == "3":
+
+            print("Existing the app. Thank you!")
+
+            break
+        
+        else: 
+
+            print("Invalid choice. Please select an valid option.")
+
+if __name__ == "__main__":
+
+    main()
+
